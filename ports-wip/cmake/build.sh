e547b4f7c7f931baca6493e548214133265b80fb
Olivier Kaloudoff <kaloudov@yandex.com>
2020-03-29 22:49:13 +0200

    ports-wip/cmake/* : stuck at "cmake needs a Fortran compiler"

--- ports-wip/cmake/build.sh
+++ ports-wip/cmake/build.sh
@@ -3,10 +3,12 @@
 set -e
 source ../../lib.sh
 TASK=fetch
-DISTVER=cmake-2.8.5
+DISTVER=cmake-3.13.0
+#DISTVER=cmake-3.13.5
+#DISTVER=cmake-3.16.5
 DISTSUFFIX=tar.gz
 
-DISTFILES="https://cmake.org/files/v2.8/$DISTVER.$DISTSUFFIX"
+DISTFILES="https://cmake.org/files/v3.13/$DISTVER.$DISTSUFFIX"
 UNPACKCOMD="tar -zxvf"
 
 package_init "$@"
@@ -15,12 +17,21 @@ package_init "$@"
 CONFIGURE_CMD="PBTARGETARCH=\"$PBTARGETARCH\"
               PREFIX=\"$PREFIX\"
               QNX_TARGET=\"$QNX_TARGET\"
-              cmake \
+              cmake --trace \
+	      -DCMAKE_REQUIRED_LIBRARIES=\"-lsocket -lelf\" \
               -DCMAKE_TOOLCHAIN_FILE=\"$EXECDIR/blackberry.toolchain.cmake\" \
               -DCMAKE_BUILD_TYPE=release ."
 
+              #-DCMAKE_USE_SYSTEM_CURL=ON \
+
+	      #-DZLIB_INCLUDE_DIRS=/usr/include/net:/usr:/usr/include
+	      #-DZLIB_ROOT=/accounts/1000/shared/documents/clitools \
+
 package_fetch
-package_patch
+cp $EXECDIR/CMakeLists.txt $WORKDIR/Utilities/cmcurl || exit 1
+cp $EXECDIR/OtherTests.cmake $WORKDIR/Utilities/cmcurl/CMake/OtherTests.cmake || exit 1
+TASK=build
+#package_patch 1
 package_build
 package_install
 package_bundle
